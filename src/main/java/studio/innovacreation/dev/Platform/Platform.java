5d40a5cc86c7362cae44be920bddb314774aee14
bobcao3 <bobcaocheng@gmail.com>
2018-06-05 18:38:08 +0800

    /wb指令打开虚拟工作台

--- src/main/java/studio/innovacreation/dev/Platform/Platform.java
+++ src/main/java/studio/innovacreation/dev/Platform/Platform.java
@@ -1,29 +1,49 @@
 package studio.innovacreation.dev.Platform;
 
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerLoginEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.command.*;
 
-public final class Platform extends JavaPlugin {
+public final class Platform extends JavaPlugin implements Listener {
 
-  @Override
-  public void onEnable() {
-    getLogger().info("Innova Platform Init");
-    WL.father = this;
-    WL.startUpdateThread();
-  }
+    @Override
+    public void onEnable() {
+        getLogger().info("Innova Platform Init");
+        WL.father = this;
+        WL.startUpdateThread();
 
-  @Override
-  public void onDisable() {
-    getLogger().info("Innova Platform disabled");
-    WL.endUpdateThread();
-  }
-
-  @Override
-  public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
-    if (cmd.getName().equalsIgnoreCase("PullWL")) {
-      WL.commandPull();
-      return true;
+        getServer().getPluginManager().registerEvents(this, this);
     }
-    return false;
-  }
+
+    @Override
+    public void onDisable() {
+        getLogger().info("Innova Platform disabled");
+        WL.endUpdateThread();
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+        if (cmd.getName().equalsIgnoreCase("PullWL")) {
+            WL.commandPull();
+            return true;
+        }
+        if (cmd.getName().equalsIgnoreCase("wb")) {
+            if (!(sender instanceof Player)) {
+                sender.sendMessage("Only players can open workbench.");
+                return false;
+            }
+
+            ((Player) sender).openWorkbench(((Player) sender).getLocation(), true);
+            return true;
+        }
+        return false;
+    }
+
+    @EventHandler
+    public void onLogin(PlayerLoginEvent event) {
+        getLogger().info( event.getPlayer().getName());
+    }
 }
